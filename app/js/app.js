52a0d3eacb42a329511410f27eecddb43cdb1e5e
zxbit2011 <zxbit2011@163.com>
2018-04-16 11:00:00 +0800

    --

---
+++ app/js/app.js
@@ -0,0 +1,8 @@
+var hiveHost = "/hive"; //"https://hive.9ee3.com";
+var thisDoMain = "https://m.9ee3.com/";
+var config = {
+    defaultPhoto: "/red_packet/img/icon/spread.png", 
+    hiveHost,
+    thisDoMain,
+    funcHandleLogGroupUrl: `${hiveHost}/account/func/handle/log/group`
+};
